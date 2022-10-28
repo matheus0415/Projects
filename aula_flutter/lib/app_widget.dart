b13777e20ec0e8752aa997fa16a4b35f59e67f48
Matheus Santos <somathsant@gmail.com>
2022-10-27 21:48:21 -0300

    First Commit

---
+++ aula_flutter/lib/app_widget.dart
@@ -0,0 +1,28 @@
+import 'package:aula_flutter/pages/home/home_page.dart';
+import 'package:aula_flutter/pages/login/login_page.dart';
+import 'package:aula_flutter/pages/splash/splash_page.dart';
+import 'package:aula_flutter/pages/lembretes/lembrete_page.dart';
+import 'package:aula_flutter/routes.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lembretes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        Routes.INITIAL: (context) => const SplashPage(),
+        Routes.LOGIN: (context) => const LoginPage(),
+        Routes.HOME: (context) => const HomePage(),
+        Routes.LEMBRETES: (context) => const LembretePage(),
+      },
+      initialRoute: Routes.INITIAL,
+    );
+  }
+}
